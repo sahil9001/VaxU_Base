85345f2382c4de88d417105c2d55cd03cc93d1b3
sahil9001 <sahilsilare@gmail.com>
2021-02-17 14:09:47 +0530

    feat: Renamed hosts and APIS

--- styles/app.js
+++ styles/app.js
@@ -1,6 +1,6 @@
-$(window).on('load', function() {
-    $('#preloader').fadeOut();
-    $('#status').delay(350).fadeOut();
+$(window).on("load", function () {
+  $("#preloader").fadeOut();
+  $("#status").delay(350).fadeOut();
 });
 
 //$(function(){
@@ -21,41 +21,51 @@ $(window).on('load', function() {
 });
 });*/
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener(
+  "DOMContentLoaded",
+  function () {
     console.log("I just ran!");
-        const webcamElement = document.getElementById('webcam');
-        const canvasElement = document.getElementById('canvas');
-        const snapSoundElement = document.getElementById('snapSound');
-        const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
-        function startWebcam() {
-            webcam.start()
-           .then(result =>{
-              console.log("webcam started");
-           })
-           .catch(err => {
-               console.log(err);
-           });
-        }
-        function takeSnap(){
-            let picture = webcam.snap();
-            document.querySelector('#download-photo').href = picture;
-        }
-
-        function flipCamera(){
-            $('#cameraFlip').click(function() {
-            webcam.flip();
-            webcam.start();  
+    const webcamElement = document.getElementById("webcam");
+    const canvasElement = document.getElementById("canvas");
+    const snapSoundElement = document.getElementById("snapSound");
+    const webcam = new Webcam(
+      webcamElement,
+      "user",
+      canvasElement,
+      snapSoundElement
+    );
+    function startWebcam() {
+      webcam
+        .start()
+        .then((result) => {
+          console.log("webcam started");
+        })
+        .catch((err) => {
+          console.log(err);
         });
-        }
+    }
+    function takeSnap() {
+      let picture = webcam.snap();
+      document.querySelector("#download-photo").href = picture;
+    }
 
-        function stopWebcam(){
-            webcam.stop();
-        }
+    function flipCamera() {
+      $("#cameraFlip").click(function () {
+        webcam.flip();
+        webcam.start();
+      });
+    }
 
-document.getElementById('button1').onclick = startWebcam;
-document.getElementById('button2').onclick = takeSnap;
-document.getElementById('button3').onclick = stopWebcam;
-}, false);
+    function stopWebcam() {
+      webcam.stop();
+    }
+
+    document.getElementById("button1").onclick = startWebcam;
+    document.getElementById("button2").onclick = takeSnap;
+    document.getElementById("button3").onclick = stopWebcam;
+  },
+  false
+);
 
 /*
 var token_ // variable will store the token
@@ -81,100 +91,106 @@ function getToken(url, clientID, clientSecret) {
 */
 /************************************************* login html ***************************************/
 var host = "sahil9001.github.io/VaxU_Base/";
-       function trylogin(){
-            var _token;
-            var userName = document.getElementById('username').value;
-            var password = document.getElementById('password').value;
-            var urlToHit = "http://20.198.3.123:80/api/users/vacc/login/";
-            var request = new XMLHttpRequest();
+function trylogin() {
+  var _token;
+  var userName = document.getElementById("username").value;
+  var password = document.getElementById("password").value;
+  var urlToHit =
+    "https://cors-anywhere.herokuapp.com/http://20.198.3.123:80/api/users/vacc/login/";
+  var request = new XMLHttpRequest();
 
-            function getToken(urlToHit, email, password){
-                var key;
-                request.open("POST", urlToHit, true);
-                request.setRequestHeader("Content-type", "application/json");
-                request.onreadystatechange = function() {
-                    if(request.readyState == 4 && request.status == 202){
-                        var response = request.responseText;
-                        var obj = JSON.parse(response);
-                        console.log(obj);
-                        key = obj.token;
-                        localStorage.setItem('token', key);
-                        console.log('token is : ' + key);
-                        success(userName);
-                    }
-                    /*else{
+  function getToken(urlToHit, email, password) {
+    var key;
+    request.open("POST", urlToHit, true);
+    request.setRequestHeader("Content-type", "application/json");
+    request.onreadystatechange = function () {
+      if (request.readyState == 4 && request.status == 202) {
+        var response = request.responseText;
+        var obj = JSON.parse(response);
+        console.log(obj);
+        key = obj.token;
+        localStorage.setItem("token", key);
+        console.log("token is : " + key);
+        success(userName);
+      }
+      /*else{
                         alert("Error : " + request.readyState);
                     }*/
-                }
-                var data = {
-                    "email"  : email,
-                    "password" : password
-                };
-                console.log(data);
-                request.send(JSON.stringify(data));
-                console.log(request.status);
-                console.log('Hi');
-                
-            }
-            //request.withCredentials = true;
-            getToken(urlToHit, userName, password);
-        }
-        function success(userName){
-            curr_url = location.href;
-            location.href = "http://" + host + "/dashboard.html";
-            document.getElementById("user_name").innerHTML = userName;
-            console.log('Hi');
-        }
-        
-        function logout(){
-            localStorage.removeItem('token');
-        }
-/**************************************** dashboard html  *******************************/
-const webcamElement = document.getElementById('webcam');
-        const canvasElement = document.getElementById('canvas');
-        const snapSoundElement = document.getElementById('snapSound');
-        const webcam = new Webcam(webcamElement, 'user', canvasElement, snapSoundElement);
-        function startWebcam() {
-            webcam.start()
-           .then(result =>{
-              console.log("webcam started");
-           })
-           .catch(err => {
-               console.log(err);
-           });
-        }
-        function takeSnap(){
-            let picture = webcam.snap();
-            document.querySelector('#download-photo').href = picture;
-        }
+    };
+    var data = {
+      email: email,
+      password: password,
+    };
+    console.log(data);
+    request.send(JSON.stringify(data));
+    console.log(request.status);
+    console.log("Hi");
+  }
+  //request.withCredentials = true;
+  getToken(urlToHit, userName, password);
+}
+function success(userName) {
+  curr_url = location.href;
+  location.href = "http://" + host + "/dashboard.html";
+  document.getElementById("user_name").innerHTML = userName;
+  console.log("Hi");
+}
 
-        function flipCamera(){
-            $('#cameraFlip').click(function() {
-            webcam.flip();
-            webcam.start();
-        });
-        }
-        function submit(){
-            var canvas = document.getElementById('canvas');
-            
-        }
-        
-        function stopWebcam(){
-            webcam.stop();
-        }
-        var urlToHit = "http://20.198.3.123/api/core/";
-        var request = new XMLHttpRequest();
-        var token = localStorage('token');
-        request.open("GET", urlToHit, true);
-        request.setRequestHeader("Content-type", "application/json");
-        request.setRequestHeader("Authorization", token);
-        request.onreadystatechange = function() {
-            if(request.readyState == 4 && request.status == 200){
-                var response = request.responseText;
-                var obj = JSON.parse(response);
-                document.getElementById('vaccinesLeft').innerHTML = obj.vaccines_available;
-                document.getElementById('priceOfVaccine').innerHTML = obj.price;
-                document.getElementById('availablityStatus').innerHTML = obj.is_available;
-            }
-        }
-        request.send();
+function logout() {
+  localStorage.removeItem("token");
+}
+/**************************************** dashboard html  *******************************/
+const webcamElement = document.getElementById("webcam");
+const canvasElement = document.getElementById("canvas");
+const snapSoundElement = document.getElementById("snapSound");
+const webcam = new Webcam(
+  webcamElement,
+  "user",
+  canvasElement,
+  snapSoundElement
+);
+function startWebcam() {
+  webcam
+    .start()
+    .then((result) => {
+      console.log("webcam started");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
+function takeSnap() {
+  let picture = webcam.snap();
+  document.querySelector("#download-photo").href = picture;
+}
+
+function flipCamera() {
+  $("#cameraFlip").click(function () {
+    webcam.flip();
+    webcam.start();
+  });
+}
+function submit() {
+  var canvas = document.getElementById("canvas");
+}
+
+function stopWebcam() {
+  webcam.stop();
+}
+var urlToHit =
+  "https://cors-anywhere.herokuapp.com/http://20.198.3.123/api/core/";
+var request = new XMLHttpRequest();
+var token = localStorage("token");
+request.open("GET", urlToHit, true);
+request.setRequestHeader("Content-type", "application/json");
+request.setRequestHeader("Authorization", token);
+request.onreadystatechange = function () {
+  if (request.readyState == 4 && request.status == 200) {
+    var response = request.responseText;
+    var obj = JSON.parse(response);
+    document.getElementById("vaccinesLeft").innerHTML = obj.vaccines_available;
+    document.getElementById("priceOfVaccine").innerHTML = obj.price;
+    document.getElementById("availablityStatus").innerHTML = obj.is_available;
+  }
+};
+request.send();
